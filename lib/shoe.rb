fd2fb935e382f170213a45a272c5dd510116a7f7
Aiden Marques <aiden.marques94@gmail.com>
2020-09-26 13:00:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    BRANDS.uniq
+    
     #instant variable starts with an @symbol. without @ ruby would think this is a local variable, not in the entire class
   end
 
